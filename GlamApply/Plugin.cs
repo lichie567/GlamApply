5c08c70914a032b1b1bae2e4df09e7713573d8ab
lichie <lichie567@protonmail.com>
2024-08-15 13:30:33 -0700

    init commit

---
+++ GlamApply/Plugin.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+
+namespace GlamApply
+{
+    public class Plugin : IDalamudPlugin
+    {
+        public static readonly string Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "0.1.0.0";
+
+        public string Name => "GlamApply";
+        private readonly ICommandManager _commandManager;
+        private readonly IPluginLog _logger;
+
+        public Plugin(ICommandManager commandManager, IPluginLog logger)
+        {
+            _commandManager = commandManager;
+            _logger = logger;
+
+            _commandManager.AddHandler(
+                "/gp",
+                new CommandInfo(PluginCommand)
+                {
+                    HelpMessage = "Apply Glamour Plate",
+                    ShowInHelp = true
+                }
+            );
+        }
+
+        private void PluginCommand(string command, string arguments)
+        {
+            arguments = arguments.Trim();
+            if (string.IsNullOrEmpty(arguments))
+            {
+                this.ResetGlamour();
+            }
+            else if (int.TryParse(arguments, out int plate))
+            {
+                this.ApplyGlamourPlate(plate);
+            }
+        }
+
+        private unsafe void ApplyGlamourPlate(int plate)
+        {
+            var instance = RaptureGearsetModule.Instance();
+            instance->EquipGearset(instance->CurrentGearsetIndex, (byte)plate);
+        }
+
+        private unsafe void ResetGlamour()
+        {
+            var instance = RaptureGearsetModule.Instance();
+            instance->EquipGearset(instance->CurrentGearsetIndex, 0);
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _commandManager.RemoveHandler("/gp");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# GlamApply
+
+![release status](https://github.com/lichie567/GlamApply/actions/workflows/release.yml/badge.svg)
+![build status](https://github.com/lichie567/GlamApply/actions/workflows/build-debug.yml/badge.svg?branch=main)
+
+GlamApply is a Dalamud plugin that lets you apply glamour plates with a chat command.
+
+Examples: 
+Typing `/gp 11` applies glamour plate 11 to your current equipped gear. 
+Typing `/gp` with no plate number will reset your glamour to the glamour plate linked to the current gearset.
+
+## How to Install
+
+GlamApply is not available in the standard Dalamud plugin repository and must be installed from my third party repository.
+
+Here is the URL for my plugin repository: `https://raw.githubusercontent.com/lichie567/GlamApply/main/repo.json`
